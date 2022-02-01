a406724001d3422d8f5a46d33d4499ee07598d53
teamweslleigarcia <wesllei.garcia@gmail.com>
2022-02-01 11:59:42 -0400

    layout pagina meus links

--- src/pages/links/links.js
+++ src/pages/links/links.js
@@ -1,8 +1,27 @@
 
+import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import './links.css'
+
 function Links() {
   return (
-    <div className="App">
-      <h1>Meus Links</h1>
+    <div className='links-container'>
+      <div className='links-header'>
+        <Link to='/'>
+          <FiArrowLeft size={38} color='#fff' />
+        </Link>
+        <h1>Meus Links</h1>
+      </div>
+      <div className='links-item'>
+        <button className='link'>
+          <FiLink size={18} color='#fff' />
+          https://www.sujeitoprogramador.com
+        </button>
+
+        <button className='link-delete'>
+          <FiTrash size={24} color='#ff5454' />
+        </button>
+      </div>
     </div>
   );
 }
